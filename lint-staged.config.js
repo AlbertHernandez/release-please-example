dd92d075ae1b554d878f7955e2bf343ee56883fe
Albert Hernandez <alberthernandezdev@gmail.com>
2024-06-29 13:48:34 +0200

    chore: remove tests

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,6 +1,6 @@
 const config = {
   "**/*.ts?(x)": () => "tsc -p tsconfig.prod.json --noEmit",
-  "*.{js,jsx,ts,tsx}": ["npm run lint", "vitest related --run"],
+  "*.{js,jsx,ts,tsx}": ["npm run lint"],
   "*.{md,json}": "prettier --write",
   "*": "npm run typos",
 };
